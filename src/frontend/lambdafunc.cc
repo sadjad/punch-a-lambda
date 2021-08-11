0d56260dc286111ff905e6ebaf3a71807bef073b
Sadjad Fouladi <sfouladi@gmail.com>
2021-08-11 12:55:03 -0700

    lambdafunc.cc: Bug fixed.

--- src/frontend/lambdafunc.cc
+++ src/frontend/lambdafunc.cc
@@ -1,4 +1,5 @@
 #include <chrono>
+#include <fcntl.h>
 #include <fstream>
 #include <iostream>
 #include <map>
@@ -13,26 +14,46 @@
 using namespace std;
 using namespace std::chrono;
 
+enum class WorkerType
+{
+  Send,
+  Recv
+};
+
 struct Worker
 {
   size_t thread_id;
   Address addr;
   TCPSocket socket {};
+  WorkerType type;
 
-  Worker( const size_t thread_id_, const Address& addr_ )
+  Worker( const size_t thread_id_,
+          const Address& addr_,
+          const WorkerType type_ )
     : thread_id( thread_id_ )
     , addr( addr_ )
+    , type( type_ )
   {
     socket.set_reuseaddr();
     socket.set_blocking( false );
   }
 };
 
-map<size_t, Address> get_peer_addresses( const uint32_t thread_id,
-                                         const string& master_ip,
-                                         const uint16_t master_port )
+string generate_random_buffer( const size_t len )
 {
-  map<size_t, Address> peers;
+  srand( time( nullptr ) );
+  string res( len, '\0' );
+  for ( size_t i = 0; i < len; i++ ) {
+    res[i] = static_cast<char>( rand() % 256 );
+  }
+  return res;
+}
+
+map<size_t, string> get_peer_addresses( const uint32_t thread_id,
+                                        const string& master_ip,
+                                        const uint16_t master_port )
+{
+  map<size_t, string> peers;
 
   string msg = "my name is:" + to_string( thread_id );
 
@@ -75,7 +96,7 @@ map<size_t, Address> get_peer_addresses( const uint32_t thread_id,
 
     cout << "peer[" << id << "]: " << ip << endl;
 
-    peers.emplace( id, Address { ip, static_cast<uint16_t>( 14000 + id ) } );
+    peers.emplace( id, ip );
   }
 
   return peers;
@@ -98,41 +119,67 @@ int main( int argc, char* argv[] )
   const uint16_t master_port = static_cast<uint16_t>( stoul( argv[2] ) );
   const uint32_t thread_id = static_cast<uint32_t>( stoul( argv[3] ) );
 
-  map<size_t, Worker> peers;
+  list<Worker> peers;
 
-  for ( auto& [peer_id, peer_addr] :
+  for ( auto& [peer_id, peer_ip] :
         get_peer_addresses( thread_id, master_ip, master_port ) ) {
-    peers.emplace( piecewise_construct,
-                   forward_as_tuple( peer_id ),
-                   forward_as_tuple( peer_id, peer_addr ) );
+    peers.emplace(
+      peers.end(),
+      peer_id,
+      Address { peer_ip, static_cast<uint16_t>( 14000 + peer_id ) },
+      WorkerType::Send );
+
+    peers.emplace(
+      peers.end(),
+      peer_id,
+      Address { peer_ip, static_cast<uint16_t>( 18000 + peer_id ) },
+      WorkerType::Recv );
   }
 
-  string send_buffer( 1 * 1024 * 1024, '\0' );
+  string send_buffer = generate_random_buffer( 1 * 1024 * 1024 );
   string read_buffer( 1 * 1024 * 1024, '\0' );
 
   size_t bytes_sent = 0;
-  size_t bytes_received = 0;
+  size_t bytes_recv = 0;
 
-  for ( auto& p : peers ) {
-    const auto peer_id = p.first;
-    Worker& peer = p.second;
+  for ( auto& peer : peers ) {
+    peer.socket.bind(
+      { "0",
+        static_cast<uint16_t>( ( peer.type == WorkerType::Send ? 18000 : 14000 )
+                               + thread_id ) } );
 
-    peer.socket.bind( { "0", static_cast<uint16_t>( 14000 + thread_id ) } );
     peer.socket.set_blocking( false );
     peer.socket.connect( peer.addr );
 
+    fout << "peer[" << peer.thread_id
+         << "]: " << ( peer.type == WorkerType::Send ? "(send)" : "(recv)" )
+         << endl;
+
     loop.add_rule(
-      "peer"s + to_string( peer_id ),
+      "peer"s + to_string( peer.thread_id ),
       peer.socket,
-      [&] { bytes_received += peer.socket.read( { read_buffer } ); },
-      [&] { return true; },
+      [&] { bytes_recv += peer.socket.read( { read_buffer } ); },
+      [&] { return peer.type == WorkerType::Send; },
       [&] { bytes_sent += peer.socket.write( send_buffer ); },
-      [&] { return peer_id == 2; } );
+      [&] { return peer.type == WorkerType::Recv; } );
   }
 
   bool terminated = false;
 
-  TimerFD termination_timer { seconds { 10 } };
+  TimerFD logging_timer { seconds { 1 } };
+
+  loop.add_rule(
+    "log",
+    Direction::In,
+    logging_timer,
+    [&] {
+      logging_timer.read_event();
+      fout << "bytes_sent=" << bytes_sent << ",bytes_recv=" << bytes_recv
+           << endl;
+    },
+    [] { return true; } );
+
+  TimerFD termination_timer { seconds { 30 } };
 
   loop.add_rule(
     "termination",
@@ -140,6 +187,11 @@ int main( int argc, char* argv[] )
     termination_timer,
     [&] { terminated = true; },
     [&] { return not terminated; } );
+
+  loop.set_fd_failure_callback( [&] {
+    fout << "socket error occurred" << endl;
+    terminated = true;
+  } );
 
   const auto start = steady_clock::now();
 
@@ -150,8 +202,8 @@ int main( int argc, char* argv[] )
   const auto end = steady_clock::now();
 
   fout << "time=" << duration_cast<milliseconds>( end - start ).count() << endl
-       << "bytes_sent=" << bytes_sent << endl
-       << "bytes_received=" << bytes_received << endl;
+       << "total_bytes_sent=" << bytes_sent << endl
+       << "total_bytes_recv=" << bytes_recv << endl;
 
   return EXIT_SUCCESS;
 }
